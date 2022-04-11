971a132c6f58920c550abbbe98d75a5cf340eb17
naikprashant1991 <naik.prashant1991@gmail.com>
2022-04-11 20:42:14 +0530

    Added Implicit and Explicit wait concepts

--- src/test/java/eCommercePlatform/ECommercePlatform.java
+++ src/test/java/eCommercePlatform/ECommercePlatform.java
@@ -8,6 +8,8 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -17,7 +19,6 @@ public class ECommercePlatform {
 		int count=0;
 		WebDriverManager.chromedriver().setup();
 		WebDriver driver = new ChromeDriver();
-		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
 		driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(10));
 		String itemsNeeded[] = {"Brocolli","Cucumber","Beetroot"};
 		
@@ -46,7 +47,18 @@ public class ECommercePlatform {
 				
 			}
 		}
-
+		
+		driver.findElement(By.xpath("//a[@class='cart-icon']")).click();
+		driver.findElement(By.xpath("//button[text()='PROCEED TO CHECKOUT']")).click();
+		driver.findElement(By.cssSelector(".promoCode")).sendKeys("rahulshettyacademy");
+		driver.findElement(By.cssSelector(".promoBtn")).click();
+		
+		//Explicit Wait
+		WebDriverWait wait = new WebDriverWait(driver,Duration.ofSeconds(5));
+		//wait.until(ExpectedConditions.visibilityOf(driver.findElement(By.cssSelector(".promoInfo"))));
+		wait.until(ExpectedConditions.visibilityOfElementLocated(By.cssSelector(".promoInfo")));
+		System.out.println(driver.findElement(By.cssSelector(".promoInfo")).getText());
+		
 	}
 
 }
